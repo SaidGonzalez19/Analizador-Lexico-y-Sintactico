62e1680a4e042196b14a5322c305dba015d30d72
SaidGonzalez <alfredjr.gonzalez.19@gmail.com>
2025-11-25 21:38:48 -0600

    Agrego carpeta src y archivo main.py

---
+++ src/main.py
@@ -0,0 +1,18 @@
+import re
+
+# Definir las expresiones regulares para los tokens
+
+
+# Función para leer y procesar el archivo
+
+
+# Función para identificar tokens en una línea
+
+
+# Función para generar archivos de salida
+
+
+# Main
+if __name__ == "__main__":
+    tokens = analizar_archivo("programa.mio")
+    generar_archivos(tokens)
